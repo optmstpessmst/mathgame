0b6d18342cd6a17e2971f899a2d7b570f3117b8a
TheMattrix <mattdarilay826@gmail.com>
2024-12-04 19:48:21 +0800

    added basic playmenu UI

---
+++ src/scripts/playtransition.gd
@@ -0,0 +1,13 @@
+extends TextureButton
+
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	pass
+	# WHAT AM I DOING
