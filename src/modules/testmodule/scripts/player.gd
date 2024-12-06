50d9403092e18725a36d5f02280b3eab369d999d
TheMattrix <mattdarilay826@gmail.com>
2024-12-06 08:33:18 +0800

    heheheha
    :3

--- src/modules/testmodule/scripts/player.gd
+++ src/modules/testmodule/scripts/player.gd
@@ -2,20 +2,30 @@ extends CharacterBody2D
 
 
 const SPEED = 400.0
-const JUMP_VELOCITY = -500.0
+const JUMP_VELOCITY = -550.0
+const DROP_VELOCITY = 5000.0
 
+const scrunkleskin = preload("res://modules/testmodule/assets/scrunkle.png")
+const evilscrunkleskin = preload("res://modules/testmodule/assets/evilscrunkle.png")
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	var evil = false
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+		
+	if Input.is_action_just_pressed("ui_down") and not is_on_floor():
+		velocity.y = DROP_VELOCITY
+	
+	##  if Input.is_action_just_pressed("evilscrunkle"):
+	##  	if evil == false:
+	##  		texture = load(evilscrunkleskin)
+	##  	else:
+	##  		texture = load(scrunkleskin)
+	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
